b5b20e76444185998f45adda41a65f2035100240
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-01-29 21:04:48 -0500

    add: canvas with click listener to draw targeting square

--- wheres-waldo-frontend/src/App.jsx
+++ wheres-waldo-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import CanvasComponent from './Canvas';
 
 function App() {
 
   return (
     <>
     <div>
-
+      <h1>Find Waldo</h1>
+      <CanvasComponent />
     </div>
     </>
   )
