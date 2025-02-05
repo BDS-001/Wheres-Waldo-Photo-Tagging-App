b9bc2d4b0eb70284c5480ffa2f137f9769cf941d
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-02-05 17:55:42 -0500

    initial commit

---
+++ backend-waldo/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config
+const express = require('express')
+const app  = express()
+const cors = require('cors');
+
+// --- Route Imports ---
+const apiRouter = require('./routes/apiRouterV1.js')
+
+// --- Middleware Setup ---
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// --- Routes ---
+app.use('/api/v1', apiRouter);
+
+if (require.main === module) {
+    const PORT = parseInt(process.env.USE_PORT, 10) || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+        console.log(`Visit: http://localhost:${PORT}/`);
+    });
+}
