8cf12a075ff17c08301371fc0d1f2409f831d120
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-02-07 17:20:03 -0500

    add: loading screen while image is loading

--- frontend-waldo/src/components/Game.jsx
+++ frontend-waldo/src/components/Game.jsx
@@ -1,13 +1,19 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import gameImage from '../assets/waldogame.png';
 import AreaSelect from './AreaSelect';
 import { useImageTransform } from '../hooks/useImageTransform';
 import { useImageInteraction } from '../hooks/useImageInteraction';
 import GameControls from './GameControls';
+import LoadingScreen from './Loading';
 
 function Game() {
     const gameAreaRef = useRef(null);
     const imgContainerRef = useRef(null);
+    const [loading, setLoading] = useState(true)
+    setTimeout(() => {
+        setLoading(false)
+        console.log("Timer finished!");
+    }, 5000);
 
     const { scale, updatePosition, zoomIn, zoomOut } = useImageTransform(gameAreaRef, imgContainerRef);
     const {
@@ -23,6 +29,7 @@ function Game() {
             <GameControls onZoomIn={zoomIn} onZoomOut={zoomOut} />
             <div className="img-container" ref={imgContainerRef}>
                 <div className="img-wrapper">
+                    {loading ? <LoadingScreen></LoadingScreen> : ''}
                     <img 
                         ref={gameAreaRef}
                         src={gameImage}
